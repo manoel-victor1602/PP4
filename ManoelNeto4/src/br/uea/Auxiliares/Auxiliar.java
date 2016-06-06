6536ee7c948a8cc815106802553d14887f10751e
Neto Oliveira <netow@myself.com>
2016-06-06 10:32:36 -0400

    adicionado questao6 e add funcao em aux

--- ManoelNeto4/src/br/uea/Auxiliares/Auxiliar.java
+++ ManoelNeto4/src/br/uea/Auxiliares/Auxiliar.java
@@ -27,6 +27,30 @@ public class Auxiliar {
 		System.out.println("");
     }
     
+    public static String stringItemProduto(ItemProduto it, Data data) {
+    		String saida="";
+        	saida += it.getProduto().getCodigo() + "\t";
+    			if (it.getProduto().getNome().length()>6){
+    				saida+=it.getProduto().getNome() + "\t";
+    			}else{
+    				saida+=it.getProduto().getNome() + "\t\t";
+    			}
+    			if( it.getProduto().getGenero().getNome().length()>6){
+    				saida+=it.getProduto().getGenero().getNome() + "\t";
+    			}else{
+    				saida+=it.getProduto().getGenero().getNome() + "\t\t";
+    			}
+    			if(it.getProduto().getMarca().getNome().length()>6){
+    				saida+=it.getProduto().getMarca().getNome() + "\tR$";
+    			}else{
+    				saida+=it.getProduto().getMarca().getNome() + "\t\tR$";
+    			}
+    			saida+=it.getProduto().getPreco() + "\t\t" +
+    						 it.getValidade()._toString() + " \t" +
+    					     it.getValidade().depois(data);
+        	return saida;
+    }
+    
     public static void mostraMenu(){
         System.out.println("#####ESTOQUE#####");
         System.out.println("COD\tNOME\t\tGENERO\t\tMARCA\t\tPRECO\t\tVALIDADE\tVALIDO?");
